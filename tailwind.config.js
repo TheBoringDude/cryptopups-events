cbb1e224c9655adf1496279c56237d31b1b3bc8f
TheBoringDude <iamcoderx@gmail.com>
2021-08-14 11:26:31 +0800

    done working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  mode: 'jit',
-  purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}'],
+  mode: "jit",
+  purge: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors,
+    fontFamily: {
+      sans: ['"Montserrat"', ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
